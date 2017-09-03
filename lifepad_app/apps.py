e2a0c5c1fd629c9005a7d77229b2143ccbdd5c4d
Pudding <nitesh.ghimire@gmail.com>
2017-09-03 09:18:42 +0530

    Revert "Add files via upload"

--- lifepad_app/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class LifepadAppConfig(AppConfig):
-    name = 'lifepad_app'
--- lifepad_app/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name="index"),
-     url(r'^lifepad_project/', views.'lifepad_project', name="lifepad_project"),
-     ]
-    
